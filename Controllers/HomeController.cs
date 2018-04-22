5ab63cd57ad09270b9825f505cbb0252f3a70be5
Kelly Williams <kellymarjorie@users.noreply.github.com>
2018-04-22 21:31:02 +1200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CucumberTechnicalTest.Models;
+using System.Globalization;
+
+namespace CucumberTechnicalTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new CurrencyConverterModel());
+        }
+
+        [HttpPost]
+        public IActionResult Result(CurrencyConverterModel model)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            model.YourNameHere = textInfo.ToTitleCase(model.YourNameHere);
+            model.ResultingText = model.ConvertNumberToString(model.NumberToConvert);
+
+            return View(model);
+        }
+    }
+}
